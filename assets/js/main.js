d155976f2467336d25cc66ec600df2e6a40b17b2
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-04-25 09:05:41 +0200

    Grundgerüst fertig

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+
+
+let openWindow = document.getElementById('openWindow');
+
+
+
+function openWindow() {
+    console.log('function runs');
+    if (openWindow == true) {
+        window.open('url(../assets/html/emailConfirm.html)');
+
+    }
+}
